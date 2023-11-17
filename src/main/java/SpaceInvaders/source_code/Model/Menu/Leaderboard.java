d62a89cb27d4060dc94df34ff59d76956edb276d
Lucas Faria <up202207540@up.pt>
2023-11-17 21:05:24 +0000

    Menu models have been made but not tested

--- src/main/java/SpaceInvaders/source_code/Model/Menu/Leaderboard.java
+++ src/main/java/SpaceInvaders/source_code/Model/Menu/Leaderboard.java
@@ -1,4 +1,18 @@
 package SpaceInvaders.source_code.Model.Menu;
 
-public class Leaderboard {
+import java.util.List;
+
+public class Leaderboard extends OnlyTextMenu {
+
+    public Leaderboard(){
+        super("src/main/java/resources/text/Leaderboard.txt");
+        sortByScore();
+    }
+    public void sortByScore(){
+        text.sort((s1, s2) -> {
+            String[] splitS1 = s1.split(" ");
+            String[] splitS2 = s2.split(" ");
+            return Integer.parseInt(splitS2[1]) - Integer.parseInt(splitS1[1]);
+                });
+    }
 }
--- src/main/java/SpaceInvaders/source_code/Model/Menu/StartMenu.java
+++ src/main/java/SpaceInvaders/source_code/Model/Menu/StartMenu.java
@@ -1,4 +1,26 @@
 package SpaceInvaders.source_code.Model.Menu;
 
-public class StartMenu {
+import java.util.Arrays;
+
+public class StartMenu extends Menu {
+
+    public StartMenu(){
+        options = Arrays.asList("Play", "Leaderboard", "Instructions", "Exit");
+    }
+
+    public boolean isSelectedPlay(){
+        return isSelected(0);
+    }
+
+    public boolean isSelectedLeaderboard(){
+        return isSelected(1);
+    }
+
+    public boolean isSelectedInstructions(){
+        return  isSelected(2);
+    }
+
+    public boolean isSelectedExit(){
+        return isSelected(3);
+    }
 }
