9ee22835e6a55ecbc51f62179168af9bdc05633a
Lucas Faria <up202207540@up.pt>
2023-11-18 18:10:15 +0000

    Basic structure for GUILanterna and testing the font

--- src/main/java/SpaceInvaders/source_code/GUI/GUILanterna.java
+++ src/main/java/SpaceInvaders/source_code/GUI/GUILanterna.java
@@ -1,4 +1,80 @@
 package SpaceInvaders.source_code.GUI;
 
-public class GUILanterna {
+import SpaceInvaders.source_code.Model.Position;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.swing.AWTTerminalFontConfiguration;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class GUILanterna implements GUI {
+    private final Screen screen;
+
+    public GUILanterna(Screen screen) {
+        this.screen = screen;
+    }
+
+    public GUILanterna(int width, int height) throws IOException, FontFormatException, URISyntaxException {
+        AWTTerminalFontConfiguration fontConfig = loadFont();
+        Terminal terminal = createTerminal(width, height, fontConfig);
+        this.screen = createScreen(terminal);
+    }
+
+    private Screen createScreen(Terminal terminal) throws IOException {
+        final Screen screen;
+        screen = new TerminalScreen(terminal);
+
+        screen.setCursorPosition(null);
+        screen.startScreen();
+        screen.doResizeIfNecessary();
+        return screen;
+    }
+
+    private Terminal createTerminal(int width, int height, AWTTerminalFontConfiguration fontConfig) throws IOException {
+        TerminalSize terminalSize = new TerminalSize(width, height + 1);
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory()
+                .setInitialTerminalSize(terminalSize);
+        terminalFactory.setForceAWTOverSwing(true);
+        terminalFactory.setTerminalEmulatorFontConfiguration(fontConfig);
+        Terminal terminal = terminalFactory.createTerminal();
+        return terminal;
+    }
+
+    private AWTTerminalFontConfiguration loadFont() throws URISyntaxException, FontFormatException, IOException {
+        URL resource = getClass().getClassLoader().getResource("fonts/Space_invaders_font.ttf");
+        File fontFile = new File(resource.toURI());
+        Font font = Font.createFont(Font.TRUETYPE_FONT, fontFile);
+
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        ge.registerFont(font);
+
+        Font loadedFont = font.deriveFont(Font.PLAIN, 25);
+        AWTTerminalFontConfiguration fontConfig = AWTTerminalFontConfiguration.newInstance(loadedFont);
+        return fontConfig;
+    }
+
+    public void drawText(int x,int y, String text, String color) {
+        TextGraphics tg = screen.newTextGraphics();
+        tg.setForegroundColor(TextColor.Factory.fromString(color));
+        tg.putString(x, y, text);
+    }
+
+    private void drawCharacter(int x, int y, char c, String color) {
+        TextGraphics tg = screen.newTextGraphics();
+        tg.setForegroundColor(TextColor.Factory.fromString(color));
+        tg.putString(x, y + 1, "" + c);
+    }
+
+    public void refresh() throws IOException {
+        screen.refresh();
+    }
 }
--- src/main/java/SpaceInvaders/source_code/Game.java
+++ src/main/java/SpaceInvaders/source_code/Game.java
@@ -1,7 +1,17 @@
 package SpaceInvaders.source_code;
 
+import SpaceInvaders.source_code.GUI.GUI;
+import SpaceInvaders.source_code.GUI.GUILanterna;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public class Game {
-    public static void main(String[] args) {
-        System.out.printf("Hello World!");
+    public static void main(String[] args) throws IOException, URISyntaxException, FontFormatException {
+        GUI testing = new GUILanterna(20,20);
+        ((GUILanterna) testing).drawText(10,10, "Lucas","#0000ff" );
+        ((GUILanterna) testing).refresh();
+
     }
 }
