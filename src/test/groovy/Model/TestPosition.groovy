2b4ef8b9407bae42eb1d3e85decdbe50ae0cd01a
gustavonelson098@gmail.com <gustavonelson098@gmail.com>
2023-11-18 03:07:11 +0000

    Finished trivial tested for simple game elements. Still need to test collectables

---
+++ src/test/groovy/Model/TestPosition.groovy
@@ -0,0 +1,25 @@
+package SpaceInvaders.source_code.Model
+
+import spock.lang.Specification
+
+class TestPosition extends Specification {
+    def "equals true"() {
+        given:
+        Position pos = new Position(2, 3)
+        Position pos2 = new Position(2, 3)
+        when:
+        boolean result = pos.equals(pos2)
+        then:
+        result == true
+    }
+
+    def "equals false"(){
+        given:
+        Position pos = new Position(3,3)
+        Position pos2 = new Position(4,4)
+        when:
+        boolean result = pos.equals(pos2)
+        then:
+        result == false
+    }
+}
