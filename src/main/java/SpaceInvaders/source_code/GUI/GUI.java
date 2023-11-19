9d350438be016353534b8e7929cb5e84614c5a0e
Lucas Faria <up202207540@up.pt>
2023-11-19 14:05:57 +0000

    Added more basic GUI elements

--- src/main/java/SpaceInvaders/source_code/GUI/GUI.java
+++ src/main/java/SpaceInvaders/source_code/GUI/GUI.java
@@ -1,4 +1,24 @@
 package SpaceInvaders.source_code.GUI;
 
+import SpaceInvaders.source_code.Model.Position;
+import com.googlecode.lanterna.input.KeyStroke;
+
+import java.io.IOException;
+
 public interface GUI {
+
+    public void drawAlien(Position position);
+    public void drawShip(Position position);
+    public void drawWall(Position position);
+    public void drawAlienShip(Position position);
+    public void drawProjectile(Position position);
+    public void drawCoverWall(Position position);
+    public void drawCollectable(Position position, String type);
+    public void drawText(Position position, String text, String color);
+
+    public KeyStroke getNextAction() throws IOException;
+    public void refresh() throws IOException;
+    public void close() throws IOException;
+
+
 }
--- src/main/java/SpaceInvaders/source_code/Game.java
+++ src/main/java/SpaceInvaders/source_code/Game.java
@@ -9,9 +9,6 @@ import java.net.URISyntaxException;
 
 public class Game {
     public static void main(String[] args) throws IOException, URISyntaxException, FontFormatException {
-        GUI testing = new GUILanterna(20,20);
-        ((GUILanterna) testing).drawText(10,10, "Lucas","#0000ff" );
-        ((GUILanterna) testing).refresh();
 
     }
 }
