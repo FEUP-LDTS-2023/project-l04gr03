a8d1bee2f86c7aed1838cc0298018b71118ee182
Lucas Faria <up202207540@up.pt>
2023-11-23 21:47:58 +0000

    Added some methods to the Game class

--- src/main/java/SpaceInvaders/source_code/Game.java
+++ src/main/java/SpaceInvaders/source_code/Game.java
@@ -1,16 +1,31 @@
 package SpaceInvaders.source_code;
 
 
+import SpaceInvaders.source_code.GUI.GUILanterna;
 import SpaceInvaders.source_code.State.GameStates;
+import SpaceInvaders.source_code.State.State;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class Game {
+    private final GUILanterna gui;
+    private final State state;
+
+    public Game() throws FontFormatException, IOException, URISyntaxException {
+        this.gui = new GUILanterna(20, 20);
+        this.state = new State();
+    }
     public static void main(String[] args) {
 
     }
 
     public void setState(GameStates gameStates) {
+        state.UpdateState(gameStates);
     }
 
     public void setPrevState() {
+        state.UpdateToPrevious();
     }
 }
