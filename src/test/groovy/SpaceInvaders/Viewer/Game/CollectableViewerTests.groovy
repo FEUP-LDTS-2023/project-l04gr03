f0129c6d343a7d1a68d96d1a7264961d9ff7a1db
Lucas Faria <up202207540@up.pt>
2023-11-30 15:41:09 +0000

    New collectableViewerClass Tested

--- src/test/groovy/SpaceInvaders/Viewer/Game/CollectableViewerTests.groovy
+++ src/test/groovy/SpaceInvaders/Viewer/Game/CollectableViewerTests.groovy
@@ -2,6 +2,9 @@ package SpaceInvaders.Viewer.Game
 
 import SpaceInvaders.GUI.GUI
 import SpaceInvaders.Model.Game.Collectables.Collectable
+import SpaceInvaders.Model.Game.Collectables.HealthCollectable
+import SpaceInvaders.Model.Game.Collectables.MachineGunModeCollectable
+import SpaceInvaders.Model.Position
 import SpaceInvaders.Viewer.Game.Collectables.CollectableViewer
 import spock.lang.Specification
 
@@ -18,7 +21,28 @@ class CollectableViewerTests extends Specification{
             1 * gui.drawElement(_,_,_)
 
     }
+    def "Health collectable draw"(){
+        given:
+            def CollectableViewer = new CollectableViewer()
+            def gui = Mock(GUI)
+            def collectable = new HealthCollectable(Mock(Position))
+        when:
+            CollectableViewer.draw(gui, collectable)
+        then:
+            1 *  gui.drawElement(collectable.getPosition(), '\u00c1', "#ff0000")
+    }
 
+    def "Other collectables draw"(){
+        given:
+            def CollectableViewer = new CollectableViewer()
+            def gui = Mock(GUI)
+            def collectable = new MachineGunModeCollectable(Mock(Position))
+        when:
+            CollectableViewer.draw(gui, collectable)
+        then:
+            1 * gui.drawElement(collectable.getPosition(), '\u0024', "#009000");
+
+    }
 
 }
 
