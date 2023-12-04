5d19d01bac0bb7e7bdf0bd614d11d0b80969f62b
Lucas Faria <up202207540@up.pt>
2023-12-04 15:06:31 +0000

    Improved the text in the instructions.txt file

--- src/main/java/SpaceInvaders/Viewer/Menu/InstructionsViewer.java
+++ src/main/java/SpaceInvaders/Viewer/Menu/InstructionsViewer.java
@@ -6,12 +6,12 @@ import SpaceInvaders.Model.Position;
 
 public class InstructionsViewer extends OnlyTextMenuViewer<Instructions> {
     public InstructionsViewer(Instructions menu){
-        super(menu, new Position(5,3));
+        super(menu, new Position(1,2));
     }
 
     @Override
     protected void drawElements(GUI gui, long time) {
         drawFileText(gui);
-        super.drawMenuTitle(gui,"INSTRUCTIONS", colorTitle, new Position(5,2));
+        super.drawMenuTitle(gui,"INSTRUCTIONS", colorTitle, new Position(1,1));
     }
 }
