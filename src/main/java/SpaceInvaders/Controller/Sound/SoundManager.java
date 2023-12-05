6982c6f4a5ba93d1db8e7dac5eb43be59d7fc3b4
Rodrigo Pires <up202207292@up.pt>
2023-12-05 17:52:31 +0000

    developing the song use druing the game

--- src/main/java/SpaceInvaders/Controller/Sound/SoundManager.java
+++ src/main/java/SpaceInvaders/Controller/Sound/SoundManager.java
@@ -7,20 +7,24 @@ import java.io.IOException;
 
 public class SoundManager {
     private static Clip shootClip;
-    private static Clip dyingsoundClip;
+    private static Clip dyingSoundClip;
     private static Clip switchOptionClip;
-
+    private static Clip backgroundMusicClip;
     static {
         try {
             AudioInputStream shootingInputStream = AudioSystem.getAudioInputStream(new File("shoot.wav"));
             shootClip = AudioSystem.getClip();
             shootClip.open(shootingInputStream);
             AudioInputStream explosionInputStream = AudioSystem.getAudioInputStream(new File("invadedkiller.wav"));
-            dyingsoundClip = AudioSystem.getClip();
-            dyingsoundClip.open(explosionInputStream);
+            dyingSoundClip = AudioSystem.getClip();
+            dyingSoundClip.open(explosionInputStream);
             AudioInputStream switchOptionInputStream = AudioSystem.getAudioInputStream(new File("light-switch-156813.mp3"));
             switchOptionClip = AudioSystem.getClip();
             switchOptionClip.open(switchOptionInputStream);
+            AudioInputStream backgroundMusicInputStream = AudioSystem.getAudioInputStream(new File("spaceinvaders1.mpeg"));
+            backgroundMusicClip = AudioSystem.getClip();
+            backgroundMusicClip.open(backgroundMusicInputStream);
+            backgroundMusicClip.loop(Clip.LOOP_CONTINUOUSLY);
         } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
             e.printStackTrace();
         }
@@ -31,7 +35,7 @@ public class SoundManager {
     }
 
     public static void playExplosionSound() {
-        playSound(dyingsoundClip);
+        playSound(dyingSoundClip);
     }
 
     public static void playSwitchOptionSound() {
